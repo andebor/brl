ca17adf51f9db781e7bab9d7715b81eea11e6d4a
andrroy <andreas@royrvik.org>
2014-11-10 16:00:00 +0100

    Done with basic sitemap

--- sitemap.php
+++ sitemap.php
@@ -2,7 +2,28 @@
 
 
 <section id="main">
-<h3>Sitemap</h3>
+	<h2>Sidekart</h2>
+
+	<ul>
+		<li><a href="/">Hjem</a></li>
+		<li><a href="/calendar/">Kalender</a></li>
+		<li><a href="/contact/">Kontakt</a></li>
+		<li><a href="#">Om borettslaget</a>
+			<ul>
+				<li><a href="/about/residents.php">Beboere</a></li>
+				<li><a href="/about/board.php">Styret</a></li>
+			</ul>
+		</li>
+		<li><a href="#">Informasjon</a>
+			<ul>
+				<li><a href="info/proceedings.php">Referater</a></li>
+				<li><a href="info/washlist.php">Vaskeliste</a></li>
+				<li><a href="info/household_regulations.php">Husordensregler</a></li>
+				<li><a href="info/projects.php">Prosjekter</a></li>
+			</ul>
+		</li>
+	</ul>
+
 </section>
 
 <?php include "includes/foot.php"; ?>
