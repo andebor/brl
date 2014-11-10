8232eef858799ad4092a7b40e0abc32423e65011
andrroy <andreas@royrvik.org>
2014-11-10 16:24:20 +0100

    Done with tentative front page

--- index.php
+++ index.php
@@ -2,7 +2,27 @@
 
 
 <section id="main">
-<h3>Hjem</h3>
+	<h2>Velkomsmelding</h2>
+
+	<p>Velkomstavsnitt eller noe i den duren? Eller kanskje det ikke trengs?</p>
+
+	<h3>Nyheter</h3>
+
+	<div class="news">
+		<h4>En nyhet</h4>
+		<p>Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. Lorem Ipsum har tålt tidens tann usedvanlig godt, og har i tillegg til å bestå gjennom fem århundrer også tålt spranget over til elektronisk typografi uten vesentlige endringer.<a href="#">Les mer</a></p>
+	</div>
+
+	<div class="news">
+		<h4>Nyhet 2</h4>
+		<p>Det er et velkjent faktum at lesere distraheres av lesbart innhold på en side når man ser på dens layout. Poenget med å bruke Lorem Ipsum er at det har en mer eller mindre normal fordeling av bokstaver i ord, i motsetning til 'Innhold her, innhold her', og gir inntrykk av å være lesbar tekst. Mange webside- og sideombrekkingsprogrammer bruker nå Lorem Ipsum som sin standard for provisorisk tekst, og et søk etter 'Lorem Ipsum' vil avdekke mang en uferdig webside. <a href="#">Les mer</a></p>
+	</div>
+
+	<div class="news">
+		<h4>Siste nyhet</h4>
+		<p>I motsetning til hva mange tror, er ikke Lorem Ipsum bare tilfeldig tekst. Dets røtter springer helt tilbake til et stykke klassisk latinsk litteratur fra 45 år f.kr., hvilket gjør det over 2000 år gammelt. Richard McClintock - professor i latin ved Hampden-Sydney College i Virginia, USA - slo opp flere av de mer obskure latinske ordene, consectetur, fra en del av Lorem Ipsum, og fant dets utvilsomme opprinnelse gjennom å studere bruken av disse ordene i klassisk litteratur. Lorem Ipsum kommer fra seksjon 1.10.32 og 1.10.33 i "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) av Cicero, skrevet i år 45 f.kr. Boken er en avhandling om teorier rundt etikk, og var veldig populær under renessansen. <a href="#">Les mer</a></p>
+	</div>
+
 </section>
 
 <?php include "includes/foot.php"; ?>
