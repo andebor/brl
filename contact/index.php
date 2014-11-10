91f84a5981b15f3f414558eb87cb73dbe72a3a97
andrroy <andreas@royrvik.org>
2014-11-10 16:14:46 +0100

    Added contact address and contact form

--- contact/index.php
+++ contact/index.php
@@ -2,7 +2,45 @@
 
 
 <section id="main">
-<h3>Kontakt</h3>
+
+	<h2>Kontakt</h2>
+
+	<h3>Postadresse</h3>
+	<address>
+		Magasinvegen Borettslag <br>
+		7028 Trondheim
+	</address>
+
+	<h3>Kontaktskjema</h3>
+
+	<form action="" method="post" id="messageForm">
+		<div class="field">
+			<label for="name">Navn <span class="required">*</span></label>
+			<input type="text" name="name" id="name" required>
+		</div>
+
+		<div class="field">
+			<label for="email">Epost <span class="required">*</span></label>
+			<input type="email" name="email" id="email" required>
+		</div>
+
+		<div class="field">
+			<label for="phone">Telefon</label>
+			<input type="text" name="phone" id="phone">
+		</div>
+		
+		<div class="field">
+			<label for="subject">Emne</label>
+			<input type="text" name="subject" id="subject">
+		</div>
+
+		<div class="field">
+			<label for="message">Melding</label>
+			<textarea name="message" id="message"></textarea>
+		</div>
+		<input type="submit" value="Submit" class="button button-primary">
+	</form>
+
 </section>
 
 <?php include "../includes/foot.php"; ?>
