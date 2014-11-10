5aeaa581c0eb6af7d1e109f63c4cf8bb56ef4833
andrroy <andreas@royrvik.org>
2014-11-10 20:27:04 +0100

    Added dummy'ish regulations to houshold regulations

--- info/household_regulations.php
+++ info/household_regulations.php
@@ -2,7 +2,18 @@
 
 
 <section id="main">
-<h3>Husordensregler</h3>
+<h2>Husordensregler</h2>
+
+<ol>
+	<li><h4>Ansvar</h4>Hver enkelt andelseier er ansvarlig for at bestemmelsene blir overholdt.</li>
+	<li><h4>Regler om ro i leilighetene</h4>Enhver plikter å overholde alminnelig krav til ro og orden.</li>
+	<li><h4>Styret</h4>Borettslaget ledes til enhver tid av et styre sammensatt og valgt slik vededtekter foreskriver.
+	Styret plikter å følge generalforsamlingens vedtak.</li>
+	<li><h4>Fellesområder</h4>Grøntanlegg som ikke er tilknyttet den enkeltes leilighet er felles eiendom og bruksområde.
+	Disse områder og all beplanting må vernes om. Uten styrets samtykke er det ikke anledning til å innlemme dleet av 
+	fellesarealene i eget område, hverken med gjereder, hekker eller beplantning.</li>
+	<li><h4>Terasser/fasader</h4>Alle endringer av bygningens fasader og farger må godkjennes av styret.</li>
+</ol>
 </section>
 
 <?php include "../includes/foot.php"; ?>
