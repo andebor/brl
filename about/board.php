d4608401bf021fc828eff24c4dc221bf041ed331
andrroy <andreas@royrvik.org>
2014-11-10 16:54:57 +0100

    Added content to board.php

--- about/board.php
+++ about/board.php
@@ -2,7 +2,41 @@
 
 
 <section id="main">
-<h3>Styret</h3>
+
+	<h2>Styret</h2>
+
+	<table class="boardTable">
+		<tr>
+			<td>Kontaktperson</td>
+			<td>Jan Jansen</td>
+		</tr>
+		<tr>
+			<td>Leder</td>
+			<td>Per Persen</td>
+		</tr>
+		<tr>
+			<td>Nestleder</td>
+			<td>Bjørn Bjørnsen</td>
+		</tr>
+		<tr>
+			<td>Styremedlem</td>
+			<td>Kjell Paulsen</td>
+		</tr>
+		<tr>
+			<td>Styremedlem</td>
+			<td>John Johnsen</td>
+		</tr>
+		<tr>
+			<td>Varamedlem</td>
+			<td>Anders Andersen</td>
+		</tr>
+		<tr>
+			<td>Varamedlem</td>
+			<td>Daniel Danielsen</td>
+		</tr>
+		
+	</table>
+
 </section>
 
 <?php include "../includes/foot.php"; ?>
