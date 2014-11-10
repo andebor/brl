96e47d4418094535fad91414a8c479aa86b52181
andebor <post@andersborud.com>
2014-11-10 16:27:29 +0100

    Change strong to h4 on months

--- calendar/index.php
+++ calendar/index.php
@@ -4,20 +4,20 @@
 <section id="main">
 <h3>Kalender</h3>
 
-<strong>Nov</strong>
+<h4>Nov</h4>
 <ul>
     <li>15. Dugnad før vintern</li>
     <li>29. Julepynting</li>
 </ul>
 
-<strong>Des</strong>
+<h4>Des</h4>
 <ul>
     <li>13. Luciadagen</li>
     <li>19. Julesamling</li>
     <li>24. Julaften</li>
 </ul>
 
-<strong>Jan</strong>
+<h4>Jan</h4>
 <ul>
     <li>5. Lorem ipsum</li>
     <li>17. eius fabellas recteque ius cu</li>
