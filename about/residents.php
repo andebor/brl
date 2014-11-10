7e479f111956eed5bdb5770e83c1e06a5698e4f7
andrroy <andreas@royrvik.org>
2014-11-10 16:42:19 +0100

    Added content to residentes

--- about/residents.php
+++ about/residents.php
@@ -2,7 +2,49 @@
 
 
 <section id="main">
-<h3>Beboere</h3>
+
+	<h2>Beboere</h2>
+	
+	<h3>Magasinvegen 3</h3>
+	<table class="residentsTable">
+		<tr>
+			<td>H0101</td>
+			<td>Jan Jansen</td>
+		</tr>
+		<tr>
+			<td>H0102</td>
+			<td>Per Persen</td>
+		</tr>
+		<tr>
+			<td>H0201</td>
+			<td>Bjørn Bjørnsen</td>
+		</tr>
+		<tr>
+			<td>H0202</td>
+			<td>Kjell Paulsen</td>
+		</tr>
+	</table>
+
+	<h3>Magasinvegen 5</h3>
+	<table class="residentsTable">
+		<tr>
+			<td>H0101</td>
+			<td>Jan Jansen</td>
+		</tr>
+		<tr>
+			<td>H0102</td>
+			<td>Per Persen</td>
+		</tr>
+		<tr>
+			<td>H0103</td>
+			<td>Bjørn Bjørnsen</td>
+		</tr>
+		<tr>
+			<td>H0104</td>
+			<td>Kjell Paulsen</td>
+		</tr>
+	</table>
+
 </section>
 
 <?php include "../includes/foot.php"; ?>
