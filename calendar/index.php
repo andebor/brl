039a7dcc6b048336003617ca390c64bd1b039aa1
andebor <post@andersborud.com>
2014-11-10 16:20:47 +0100

    Added content to calendar

--- calendar/index.php
+++ calendar/index.php
@@ -3,6 +3,25 @@
 
 <section id="main">
 <h3>Kalender</h3>
+
+<strong>Nov</strong>
+<ul>
+    <li>15. Dugnad før vintern</li>
+    <li>29. Julepynting</li>
+</ul>
+
+<strong>Des</strong>
+<ul>
+    <li>13. Luciadagen</li>
+    <li>19. Julesamling</li>
+    <li>24. Julaften</li>
+</ul>
+
+<strong>Jan</strong>
+<ul>
+    <li>5. Lorem ipsum</li>
+    <li>17. eius fabellas recteque ius cu</li>
+</ul>
 </section>
 
 <?php include "../includes/foot.php"; ?>
