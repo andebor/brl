d961001c57fc5d1c86ca7e576946ab69ea773b55
andrroy <andreas@royrvik.org>
2014-11-10 20:09:34 +0100

    Added dummy content to washlist

--- info/washlist.php
+++ info/washlist.php
@@ -2,7 +2,83 @@
 
 
 <section id="main">
-<h3>Vaskeliste</h3>
+
+	<h2>Vaskeliste</h2>
+
+	<h3>Vaskelist 1. etasje</h3>
+
+	<table class="washListTable">
+		<tr>
+			<th>Ukenummer</th>
+			<th>Navn</th>
+		</tr>
+		<tr>
+			<td>Uke 47</td>
+			<td>Jan Jansen</td>
+		</tr>
+		<tr>
+			<td>Uke 48</td>
+			<td>Per Persen</td>
+		</tr>
+		<tr>
+			<td>Uke 49</td>
+			<td>Bjørn Bjørnsen</td>
+		</tr>
+		<tr>
+			<td>Uke 50</td>
+			<td>Kjell Paulsen</td>
+		</tr>
+		<tr>
+			<td>Uke 51</td>
+			<td>John Johnsen</td>
+		</tr>
+		<tr>
+			<td>Uke 52</td>
+			<td>Anders Andersen</td>
+		</tr>
+		<tr>
+			<td>Uke 1</td>
+			<td>Daniel Danielsen</td>
+		</tr>
+	</table>
+
+	<h3>Vaskelist 3. etasje</h3>
+
+	<table class="washListTable">
+		<tr>
+			<th>Ukenummer</th>
+			<th>Navn</th>
+		</tr>
+		<tr>
+			<td>Uke 47</td>
+			<td>Jan Jansen</td>
+		</tr>
+		<tr>
+			<td>Uke 48</td>
+			<td>Per Persen</td>
+		</tr>
+		<tr>
+			<td>Uke 49</td>
+			<td>Bjørn Bjørnsen</td>
+		</tr>
+		<tr>
+			<td>Uke 50</td>
+			<td>Kjell Paulsen</td>
+		</tr>
+		<tr>
+			<td>Uke 51</td>
+			<td>John Johnsen</td>
+		</tr>
+		<tr>
+			<td>Uke 52</td>
+			<td>Anders Andersen</td>
+		</tr>
+		<tr>
+			<td>Uke 1</td>
+			<td>Daniel Danielsen</td>
+		</tr>
+	</table>
+
 </section>
 
 <?php include "../includes/foot.php"; ?>
