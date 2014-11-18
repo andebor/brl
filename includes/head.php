47b9966e7cad7c0b3c57c1e27d09f339eeeeaee6
Morten Noddeland <morten@noddeland.no>
2014-11-18 19:27:52 +0100

    Added picture of the building

--- includes/head.php
+++ includes/head.php
@@ -17,18 +17,7 @@
         <ul class="nav navbar-nav">
           <li><a href="/"><i class="fa fa-home"></i></a></li>
           <li><a href="/calendar/">Kalender</a></li>
-          <li><a href="/contact/">Kontakt</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Om oss <i class="fa fa-chevron-down"></i></a>
-            <ul class="dropdown-menu" role="menu">
-              <li class="dropdown-item">
-                <a href="/about/residents.php">Beboere</a>
-              </li>
-              <li class="dropdown-item">
-                <a href="/about/board.php">Styret</a>
-              </li>
-            </ul>
-          </li>
+
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Informasjon <i class="fa fa-chevron-down"></i></a>
             <ul class="dropdown-menu" role="menu">
@@ -46,6 +35,20 @@
               </li>
             </ul>
           </li>
+
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Om oss <i class="fa fa-chevron-down"></i></a>
+            <ul class="dropdown-menu" role="menu">
+              <li class="dropdown-item">
+                <a href="/about/residents.php">Beboere</a>
+              </li>
+              <li class="dropdown-item">
+                <a href="/about/board.php">Styret</a>
+              </li>
+            </ul>
+          </li>
+
+          <li><a href="/contact/">Kontakt</a></li>
         </ul>
       </nav>
     </div>
--- index.php
+++ index.php
@@ -6,30 +6,32 @@ include "includes/head.php"; ?>
   <div class="col-2-3">
     <section id="main" class="panel">
 
-    <h3>Nyheter</h3>
+    <h2><i class="fa fa-newspaper-o"></i> Nyheter</h2>
 
     <div class="news">
-      <h4>En nyhet</h4>
-      <p>Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. Lorem Ipsum har tålt tidens tann usedvanlig godt, og har i tillegg til å bestå gjennom fem århundrer også tålt spranget over til elektronisk typografi uten vesentlige endringer.<a href="#">Les mer</a></p>
+      <h3>En nyhet</h3>
+      <p>Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok. Lorem Ipsum har tålt tidens tann usedvanlig godt, og har i tillegg til å bestå gjennom fem århundrer også tålt spranget over til elektronisk typografi uten vesentlige endringer. <a href="#">Les mer</a></p>
     </div>
 
     <div class="news">
-      <h4>Nyhet 2</h4>
+      <h3>Nyhet 2</h3>
       <p>Det er et velkjent faktum at lesere distraheres av lesbart innhold på en side når man ser på dens layout. Poenget med å bruke Lorem Ipsum er at det har en mer eller mindre normal fordeling av bokstaver i ord, i motsetning til 'Innhold her, innhold her', og gir inntrykk av å være lesbar tekst. Mange webside- og sideombrekkingsprogrammer bruker nå Lorem Ipsum som sin standard for provisorisk tekst, og et søk etter 'Lorem Ipsum' vil avdekke mang en uferdig webside. <a href="#">Les mer</a></p>
     </div>
 
     <div class="news">
-      <h4>Siste nyhet</h4>
+      <h3>Siste nyhet</h3>
       <p>I motsetning til hva mange tror, er ikke Lorem Ipsum bare tilfeldig tekst. Dets røtter springer helt tilbake til et stykke klassisk latinsk litteratur fra 45 år f.kr., hvilket gjør det over 2000 år gammelt. Richard McClintock - professor i latin ved Hampden-Sydney College i Virginia, USA - slo opp flere av de mer obskure latinske ordene, consectetur, fra en del av Lorem Ipsum, og fant dets utvilsomme opprinnelse gjennom å studere bruken av disse ordene i klassisk litteratur. Lorem Ipsum kommer fra seksjon 1.10.32 og 1.10.33 i "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) av Cicero, skrevet i år 45 f.kr. Boken er en avhandling om teorier rundt etikk, og var veldig populær under renessansen. <a href="#">Les mer</a></p>
     </div>
   </section>
   </div>
 
   <div class="col-1-3">
-    <aside class="panel">
-      <h2>Velkomsmelding</h2>
-      <p>Velkomstavsnitt eller noe i den duren? Eller kanskje det ikke trengs?</p>
-      <p>Det er et velkjent faktum at lesere distraheres av lesbart innhold på en side når man ser på dens layout. Poenget med å bruke Lorem Ipsum er at det har en mer eller mindre normal fordeling av bokstaver i ord, i motsetning til 'Innhold her, innhold her', og gir inntrykk av å være lesbar tekst. Mange webside- og sideombrekkingsprogrammer bruker nå Lorem Ipsum som sin standard for provisorisk tekst, og et søk etter 'Lorem Ipsum' vil avdekke mang en uferdig webside. </p>
+    <aside class="panel no-padding">
+      <img src="/assets/img/building.jpg" alt="Bilde av borettslaget">
+      <div class="padding">
+        <h2>Om borettslaget</h2>
+        <p>Det er et velkjent faktum at lesere distraheres av lesbart innhold på en side når man ser på dens layout. Poenget med å bruke Lorem Ipsum er at det har en mer eller mindre normal fordeling av bokstaver i ord, i motsetning til 'Innhold her, innhold her', og gir inntrykk av å være lesbar tekst. Mange webside- og sideombrekkingsprogrammer bruker.</p>
+      </div>
     </aside>
   </div>
 </div>
